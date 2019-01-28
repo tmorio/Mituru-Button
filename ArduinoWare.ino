90ea221e71ae31636e7a260181a0a02315225ff4
Takenori Morio <morikapusan@eleken.jp>
2019-01-28 18:10:27 +0900

    fix:Comp Error

--- ArduinoWare.ino
+++ ArduinoWare.ino
@@ -4,16 +4,15 @@
 
 void setup() {
   Keyboard.begin();
-  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
-  if(digitalRead(1) == LOW){
+  if(digitalRead(2) == LOW){
     for(int counter = 0; 0 < 100; counter++){
         Keyboard.press(VolUp);
         delay(100);
     }
-    counter = 0;
     Keyboard.releaseAll();
 
     while(digitalRead(1) == LOW);
